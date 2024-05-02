7210431318ef8170164893b75d0ec4c19e03ca65
Miguel <migielcarmo21@gmail.com>
2024-05-02 11:02:08 -0300

    Criando a função adicionar tarefa

--- src/js/script.js
+++ src/js/script.js
@@ -10,5 +10,24 @@ function validaCampo()
     if(document.getElementById("task").value =="")
     valida =true
     return valida;
-    
+
+}
+//Função que adiciona a tarefa
+
+function adcionarTarefa(){
+        //criando a variavel que vai receber as tarefas
+    let linhas =document.getElementById("task");
+
+    if(validaCampo()){
+        alert("Preencha o campo com a tarefa")
+    }else{
+        //adicionando as tarefas
+        tarefas.push(linha.value);
+        //passando para a proxima linha vazia 
+        linhas.value="";
+        //chamando a função que vai mostarar todas as tarefas
+        listarTarefas();
+    }
+    //volta para o input da tarefa
+    document.getElementById("task").fucus();
 }
